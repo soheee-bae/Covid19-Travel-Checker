e6fa41592a5ca9e9a86726000cb9d78b6de09147
milosticklefrizzell <milosticklefrizzell@gmail.com>
2020-10-10 15:25:49 -0800

    Added in react-chart implementation (still needs to be fed proper values) and styled some CSS. Work done in Secondpage.js/css

--- client/src/component/Secondpage.js
+++ client/src/component/Secondpage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "../styles/Home.css";
+import { Chart } from 'react-charts'
+import "../styles/Secondpage.css";
 import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
@@ -24,6 +25,88 @@ const Secondpage = (props) => {
   const [death, setDeath] = useState("");
   const [stateName, setStateName] = useState(selectedState);
   const [stateAbrev, setStateAbrev] = useState("");
+
+  const customStyles = {
+    control: (base, state) => ({
+      ...base,
+      fontSize: 16,
+      border: state.isFocused ? 0 : 0,
+      boxShadow: state.isFocused ? 0 : 0,
+      cursor: "text",
+      borderRadius: 5,
+      padding: 4,
+    }),
+
+    option: (styles, { isFocused }) => {
+      return {
+        ...styles,
+        cursor: "pointer",
+        backgroundColor: isFocused ? "white" : "white",
+        color: isFocused ? "rgba(255, 80, 86)" : "black",
+        lineHeight: 2,
+        fontSize: 14,
+      };
+    },
+
+    input: (styles) => ({
+      ...styles,
+      color: "black",
+    }),
+
+    menu: (styles) => ({
+      ...styles,
+      marginTop: 0,
+      boxShadow: "none",
+      borderRadius: 0,
+      borderTop: "solid 1px",
+    }),
+
+    singleValue: (styles) => ({
+      ...styles,
+      color: "rgba(255, 80, 86)",
+    }),
+  };
+
+  const data = React.useMemo(
+    () => [
+      {
+        label: 'Confirmed',
+        data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+      },
+      {
+        label: 'Tested',
+        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+      },
+      {
+        label: 'Recovered',
+        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+      },
+      {
+        label: 'Deaths',
+        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+      }
+    ],
+    []
+  )
+ 
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'utc', position: 'bottom' },
+      { type: 'linear', position: 'left' }
+    ],
+    []
+  )
+ 
+  const lineChart = (
+    <div
+      style={{
+        width: '500px',
+        height: '305px'
+      }}
+    >
+      <Chart data={data} axes={axes} />
+    </div>
+  )
 
   
   var x = 0;
@@ -117,10 +200,29 @@ const Secondpage = (props) => {
     //Use the 'styles/Secondpage.css' to style this page.
     <div className="Secondpage">
       <div className="mainContent">
+        
+              {/*This section is just for the arrow to previous and next pages*/}
+      <div className="arrow-icon">
+          
+      <Link to="/" style={{ textDecoration: "none" }}>
+          
+        <FontAwesomeIcon icon={faAngleLeft} />
+      </Link>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Link
+        to="/restrictions-on-travelers"
+        style={{ textDecoration: "none" }}
+      >
+        <FontAwesomeIcon icon={faAngleRight} />
+      </Link>
+    </div>
+
         <div>Updated Time : {dateChecked}</div>
         <div>
           {/* COVID-19 dashboard here */}
           <h1>State: {stateName}</h1>
+
+        
           <h3>
             Confirmed :
             <FontAwesomeIcon className="arrow-up" icon={faArrowUp} />
@@ -148,20 +250,11 @@ const Secondpage = (props) => {
         </div>
       </div>
 
-      {/*This section is just for the arrow to previous and next pages*/}
-      <div className="leftarrow">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <FontAwesomeIcon icon={faAngleLeft} />
-        </Link>
+      <div className="graph">
+      {lineChart}
       </div>
-      <div className="rightarrow">
-        <Link
-          to="/restrictions-on-travelers"
-          style={{ textDecoration: "none" }}
-        >
-          <FontAwesomeIcon icon={faAngleRight} />
-        </Link>
-      </div>
+
+
     </div>
   );
 };
