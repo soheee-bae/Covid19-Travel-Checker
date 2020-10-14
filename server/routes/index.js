cfc5de6e6df3ef853cf122b4509405f772bdca49
Akourne Bursche <crocodoctopus@gmail.com>
2020-10-13 15:00:36 -1000

    add jwt

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,111 +1,123 @@
-var express = require("express");
-var bcrypt = require("bcrypt");
-var mongoose = require("mongoose");
+var express = require('express');
+var bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken')
 
 var router = express.Router();
+router.use(express.json());
 
-mongoose.connect("mongodb://localhost/my_db", {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+const jwtKey = "0f9841176d94679a9ba9c9165ceb713e73c5de54c461ac3bf1a9545c26504fae3a6e3cb9f1b1652f5422777c6a4b82e5bd299bffb45aa71cbac273c39c5d3965";
+
+mongoose.connect('mongodb://localhost/my_db');
 var account = mongoose.Schema({
-  username: String,
-  password: String,
-  salt: String,
+   username: String,
+   password: String,
+   salt: String,
 });
 var Account = mongoose.model("Account", account);
 
-Account.remove({}, (req, result) => {});
+Account.remove({}, (req, result) => { });
 
-router.post("/login", (req, res) => {
-  var username = req.body.username;
-  var password = req.body.password;
+router.post('/login', (req, res) => {
+	var username = req.body.username;
+	var password = req.body.password;
 
-  const extractData = () =>
-    new Promise((resolve, reject) => {
-      Account.findOne({ username: username }, (err, doc) => {
-        if (err) return reject(err);
-        if (doc == null) return reject("Username does not exist");
-        return resolve([doc.password, doc.salt]);
-      });
-    });
+	if (username == null || password == null)
+		return res.sendStatus(404);
 
-  const hash = ([dbhash, salt]) =>
-    new Promise((resolve, reject) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) return reject(err);
-        return resolve([dbhash, hash]);
-      });
-    });
+	const extractData = () => new Promise((resolve, reject) => {
+		Account.findOne({ username: username }, (err, doc) => {
+			if (err) return reject(err);
+			if (doc == null) {
+				console.log("no exist reject");
+				return reject("Username does not exist")
+			};
+			return resolve([doc.password, doc.salt]);
+		});
+	});
 
-  const compare = ([dbhash, hash]) =>
-    new Promise((resolve, reject) => {
-      if (dbhash != hash) return reject("Incorrect password");
-      return resolve(null);
-    });
+	const hash = ([dbhash, salt]) => new Promise((resolve, reject) => {
+		bcrypt.hash(password, salt, (err, hash) => {
+			if (err) return reject(err);
+			return resolve([dbhash, hash]);
+		});
+	});
 
-  extractData()
-    .then(hash)
-    .then(compare)
-    .then((_) => res.send("Login valid"))
-    .catch((err) => res.send(404, "Login invalid: " + err));
+	const compare = ([dbhash, hash]) => new Promise((resolve, reject) => {
+		if (dbhash != hash) return reject("Incorrect password");
+		return resolve(null);
+	});
+
+	const genJwt = (_) => new Promise((resolve, reject) => {
+		console.log("genJwt");
+		return resolve(jwt.sign({ username }, jwtKey));
+	});
+
+	const respond = (jwt) => new Promise((resolve, reject) => {
+		console.log("respond");
+		res.send(jwt);
+		return resolve(null);
+	});
+
+	extractData()
+		.then(hash)
+		.then(compare)
+		.then(genJwt)
+		.then(respond)
+		.catch((err) => res.sendStatus(404));
 });
 
-router.post("/register", (req, res) => {
-  var username = req.body.username;
-  var password = req.body.password;
+router.post('/register', (req, res) => {
+	var username = req.body.username;
+	var password = req.body.password;
 
-  const verifyUnique = () =>
-    new Promise((resolve, reject) => {
-      Account.findOne({ username: username }, (err, doc) => {
-        if (err) return reject(err);
-        if (doc != null) return reject("Username already exists");
-        return resolve(null);
-      });
-    });
+	if (username == null || password == null)
+		return res.sendStatus(404);
 
-  const salt = (_) =>
-    new Promise((resolve, reject) => {
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) return reject(err);
-        return resolve(salt);
-      });
-    });
+	const verifyUnique = () => new Promise((resolve, reject) => {
+		Account.findOne({username: username}, (err, doc) => {
+			if (err) return reject(err);
+			if (doc != null) return reject("Username already exists");
+			return resolve(null);
+		});
+	});
 
-  const hash = (salt) =>
-    new Promise((resolve, reject) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) return reject(err);
-        return resolve([username, hash, salt]);
-      });
-    });
+	const hash = (salt) => new Promise((resolve, reject) => {
+		bcrypt.genSalt(10, (err, salt) => {
+			if (err) return reject(err);
+			bcrypt.hash(password, salt, (err, hash) => {
+				if (err) return reject(err);
+				return resolve([hash, salt]);
+			});
+		});
+	});
 
-  const dbInsert = ([username, hash, salt]) =>
-    new Promise((resolve, reject) => {
-      return new Account({
-        username: username,
-        password: hash,
-        salt: salt,
-      }).save((err, account) => {
-        if (err) return reject(err);
-        return resolve(account);
-      });
-    });
+	const dbInsert = ([hash, salt]) => new Promise((resolve, reject) => {
+		return new Account({
+			username: username,
+			password: hash,
+			salt: salt,
+		}).save((err, account) => {
+			if (err) return reject(err);
+			return resolve(null);
+		});
+	});
 
-  const respond = (_) =>
-    new Promise((resolve, reject) => {
-      res.send("Register success");
-      return resolve(null);
-    });
+	const genJwt = (_) => new Promise((resolve, reject) => {
+		return resolve(jwt.sign({ username }, jwtKey));
+	});
 
-  verifyUnique(username)
-    .then(salt)
-    .then(hash)
-    .then(dbInsert)
-    .then((_) => res.send("Register success"))
-    .catch((err) => res.send(404, "Register fail: " + err));
+	const respond = (jwt) => new Promise((resolve, reject) => {
+		res.send(jwt);
+		return resolve(null);
+	});
+
+	verifyUnique(username)
+		.then(hash)
+		.then(dbInsert)
+		.then(genJwt)
+		.then(respond)
+		.catch((err) => res.sendStatus(404));
 });
 
 module.exports = router;
