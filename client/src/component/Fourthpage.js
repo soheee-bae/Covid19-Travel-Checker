cbf03467940ff500b2345994d3a8412fe6b685fa
sohee28 <amybae28@gmail.com>
2020-11-05 20:07:33 -0900

    [finishes #174837325] Display the list of counties with drop down

--- client/src/component/Fourthpage.js
+++ client/src/component/Fourthpage.js
@@ -1,22 +1,22 @@
-import React, {useContext}  from "react";
+import React, { useContext } from "react";
 import "../styles/Fourthpage.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import {stateContext} from '../App'
+import { stateContext } from "../App";
 import restrictions from "../restrictions/restrictions.json";
 
 const Fourthpage = () => {
-  const {selectedState, setSelectedState} = useContext(stateContext);
-  
+  const { selectedState, setSelectedState } = useContext(stateContext);
+
   return (
     //This is the Fourth page. It will have Travel restrictions for chosen state.
     //Use the 'styles/Fourthpage.css' to style this page.
     <div className="Fourthpage">
       <div className="mainContent">
-          <h1>Travel Restrictions</h1>
-          <h2>{selectedState}</h2>
+        <h1>Travel Restrictions</h1>
+        <h2>{selectedState}</h2>
       </div>
 
       {/*This section is just for the arrow to previous and next pages*/}
--- client/src/component/Testsitepage.js
+++ client/src/component/Testsitepage.js
@@ -1,30 +1,117 @@
-import React,{useContext}  from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "../styles/Testsitepage.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-import {stateContext} from '../App'
+import Testsite from "../testsite/Testsite.json";
+import Select from "react-select";
+import { stateContext } from "../App";
 
 const Testsitepage = () => {
-  const {selectedState, setSelectedState} = useContext(stateContext);
+  const { selectedState, setSelectedState } = useContext(stateContext);
+  const [testsite, setTestsite] = useState({});
+  const [Counties, setCounties] = useState([]);
+  var CountiesList = [];
+
+  useEffect(() => {
+    Testsite.map((testsite) => {
+      if ((testsite.State = selectedState)) {
+        setTestsite(testsite);
+        setCounties(testsite.Counties);
+      }
+    });
+  }, []);
+
+  const SearchbarStyle = {
+    control: (base, state) => ({
+      ...base,
+      fontSize: 14,
+      border: state.isFocused ? 0 : 0,
+      cursor: "text",
+      borderRadius: 5,
+      width: "40vw",
+      minWidth: "300px",
+      height: "3rem",
+      padding: "0 1rem",
+      boxShadow: "0 10px 35px rgba(0,0,0,.2)",
+      color: "#6c757d",
+      outline: "none",
+    }),
+    option: (styles, { isFocused }) => {
+      return {
+        ...styles,
+        cursor: "pointer",
+        backgroundColor: isFocused ? "white" : "white",
+        color: isFocused ? "rgba(255, 80, 86)" : "black",
+        lineHeight: 2,
+        fontSize: 14,
+        width: "40vw",
+        height: "3rem",
+        minWidth: "300px",
+        zIndex: "0",
+      };
+    },
+
+    input: (styles) => ({
+      ...styles,
+      color: "black",
+      width: "40vw",
+      fontSize: 14,
+      minWidth: "300px",
+      zIndex: "0",
+    }),
+
+    menu: (styles) => ({
+      ...styles,
+      boxShadow: "none",
+      borderRadius: 0,
+      borderTop: "solid 1px",
+      width: "40vw",
+      height: "3rem",
+      minWidth: "300px",
+      zIndex: "0",
+    }),
+
+    singleValue: (styles) => ({
+      ...styles,
+      color: "rgba(255, 80, 86)",
+      width: "40vw",
+      minWidth: "300px",
+      zIndex: "0",
+    }),
+  };
+
+  const searchList = Counties.forEach((county) => {
+    CountiesList.push({ label: county, value: county });
+  });
 
   return (
-    //This is the Test site location page. It will have test site locations for chosen state.
-    //Use the 'styles/Testsitepage.css' to style this page.
     <div className="Testsitepage">
-      <div className="mainContent">
-          <h1>Test Site Locations</h1>
-          <h2>{selectedState}</h2>
-      </div>
-
-      {/*This section is just for the arrow to previous and next pages*/}
-      <div className="leftarrow">
+      <div className="Testsite-leftarrow">
         <Link
           to="/state-travel-restrictions"
           style={{ textDecoration: "none" }}
         >
-          <FontAwesomeIcon icon={faAngleLeft} />
+          <FontAwesomeIcon
+            style={{ fontSize: "30px", color: "black" }}
+            icon={faAngleLeft}
+          />
         </Link>
+      </div>
+      <div className="Testsitepage-mainarea">
+        <div className="search-bar-for-counties">
+          <Select
+            options={CountiesList}
+            placeholder="Search the Counties"
+            openMenuOnClick={false}
+            className="counties-search-bar"
+            styles={SearchbarStyle}
+          />
+        </div>
+        <div className="output-for-testing-site"></div>
+      </div>
+      <div className="right-arrow-icon">
+        <p></p>
       </div>
     </div>
   );
