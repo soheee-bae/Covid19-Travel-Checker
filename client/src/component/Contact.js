f78bc8067ee2333e1d3f3a0cb3235436289e55a5
milosticklefrizzell <milosticklefrizzell@gmail.com>
2020-11-30 23:27:15 -0900

    Added functionality and content for contact, about pages slight changes to education page

--- client/src/component/Contact.js
+++ client/src/component/Contact.js
@@ -5,7 +5,24 @@ const Contact = () => {
   return (
     //This is the Contact page.
     //Use the 'styles/Contact.css' to style this page. 
-    <div className="Homepage">Contact us page</div>
+    <div className="Contact">
+    <div className="mainContent-area">
+    <h1>Contact Information</h1>
+    <div id="boxText">
+    <h4>Do you have questions or bugs found relating to this website?</h4>
+    <br></br>
+    <h4>Or are you curious about this project?</h4>
+    <br></br>
+    <h4>You can reach us via the information below: </h4>
+    <br></br>
+    </div>
+    <div id="box">
+    <a href="url"><h3>fireferrets@gmail.com</h3></a>
+    <h3>(999) 999-9999</h3>
+    <h3>520 West Fire Ferret Avenue, Anchorage AK, 99503</h3>
+    </div>
+    </div>
+    </div>
   );
 };
 
