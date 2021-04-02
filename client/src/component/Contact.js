dfa68cf32178e56afa7a0f6ca09ae034157f3b20
sohee28 <amybae28@gmail.com>
2021-04-01 18:17:24 -0800

    final version

--- client/src/component/Contact.js
+++ client/src/component/Contact.js
@@ -4,24 +4,26 @@ import "../styles/Contact.css";
 const Contact = () => {
   return (
     //This is the Contact page.
-    //Use the 'styles/Contact.css' to style this page. 
+    //Use the 'styles/Contact.css' to style this page.
     <div className="Contact">
-    <div className="mainContent-area">
-    <h1>Contact Information</h1>
-    <div id="box">
-    <h4>Do you have questions or bugs found relating to this website?</h4>
-    <br></br>
-    <h4>Or are you curious about this project?</h4>
-    <br></br>
-    <h4>You can reach us via the information below: </h4>
-    <br></br>
-    </div>
-    <div id="box">
-    <a href="url"><h3>fireferrets@gmail.com</h3></a>
-    <h3>(999) 999-9999</h3>
-    <h3>520 West Fire Ferret Avenue, Anchorage AK, 99503</h3>
-    </div>
-    </div>
+      <div className="mainContent-area">
+        <h1>Contact Information</h1>
+        <div id="box">
+          <h4>Do you have questions or bugs found relating to this website?</h4>
+          <br></br>
+          <h4>Or are you curious about this project?</h4>
+          <br></br>
+          <h4>You can reach us via the information below: </h4>
+          <br></br>
+        </div>
+        <div id="box">
+          <a href="url">
+            <h3>fireferrets@gmail.com</h3>
+          </a>
+          <h3>(999) 999-9999</h3>
+          <h3>520 West Fire Ferret Avenue, Anchorage AK, 99503</h3>
+        </div>
+      </div>
     </div>
   );
 };
